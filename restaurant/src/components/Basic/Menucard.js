c74d4ce430721c561d87553814227eda2c65dc4a
tejawvini3494 <tejaswinivirshiav1@gmail.com>
2024-05-28 23:38:40 +0530

    structure and set style

--- restaurant/src/components/Basic/Menucard.js
+++ restaurant/src/components/Basic/Menucard.js
@@ -1,37 +1,33 @@
 import React from "react";
 
-export default function Menucard({ menudata}) {
-
+export default function Menucard({ menudata }) {
   return (
     <>
-   
+    <div className="card-section">
       {menudata.map((curElem) => {
         return (
           <>
             <div className="card-container" key={curElem.id}>
               <div className="card">
                 <div className="card-body">
-                  <span className="card-number card-circle subtle">{curElem.id}</span>
+                  <span className="card-number card-circle subtle">
+                    {curElem.id}
+                  </span>
                   <span className="card-author sbtle">{curElem.name}</span>
                   <h2 className="card-title">{curElem.name}</h2>
                   <span className="card-description subtle">
                     {curElem.description}
                   </span>
                 </div>
-                <img src={curElem.img}
-                  alt=""
-                  className="card-media"
-                />
+                <img src={curElem.img} alt="" className="card-media" />
                 <span className="card-tag subtle">Order Now</span>
               </div>
             </div>
-            
-            
+           
           </>
-       
         );
       })}
-   
+       </div>
     </>
   );
 }
--- restaurant/src/components/Basic/Navbar.js
+++ restaurant/src/components/Basic/Navbar.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 
-export default function Navbar({filterItem, menulist} ) {
+export default function Navbar({ filterItem, menulist }) {
   return (
     <>
-     <nav className="navbar">
-      <div className="nav-btn">
-
-        {menulist.map((curElem)=>{
-            return(
-<>
-<button className="btn-group-item" onClick={()=>filterItem(curElem)}>{curElem}</button>
-</>
-            )
-        })}
-        
-      </div>
-    </nav>
+      <nav className="navbar">
+        <div className="nav-btn">
+          {menulist.map((curElem) => {
+            return (
+              <>
+                <button
+                  className="btn-group-item"
+                  onClick={() => filterItem(curElem)}
+                >
+                  {curElem}
+                </button>
+              </>
+            );
+          })}
+        </div>
+      </nav>
     </>
-  )
+  );
 }
